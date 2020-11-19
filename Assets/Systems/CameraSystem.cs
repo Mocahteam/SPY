7d410eece6dc0090797e173406e3269888042ed5
Jean-Baptiste LY <ly.jeanbaptiste@gmail.com>
2020-11-19 10:24:31 +0100

    enlève une fonction inutile

--- Assets/Systems/CameraSystem.cs
+++ Assets/Systems/CameraSystem.cs
@@ -84,14 +84,4 @@ public class CameraSystem : FSystem {
 		}
 		
 	}
-	
-	private static float ClampAngle(float angle, float min, float max)
-    {
-        if (angle < -360)
-            angle += 360;
-        if (angle > 360)
-            angle -= 360;
-        return Mathf.Clamp(angle, min, max);
-    }
-    
 }
