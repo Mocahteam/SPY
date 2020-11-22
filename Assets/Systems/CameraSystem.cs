4002de4e5356f9e736c9c417a78605b0794b7342
Jean-Baptiste LY <ly.jeanbaptiste@gmail.com>
2020-11-22 23:50:44 +0100

    variables plus générales pour les limites de position (caméra)

--- Assets/Systems/CameraSystem.cs
+++ Assets/Systems/CameraSystem.cs
@@ -21,6 +21,12 @@ public class CameraSystem : FSystem {
 	{
         go.GetComponent<CameraComponent>().orbitH = go.GetComponent<CameraComponent>().transform.eulerAngles.y;
         go.GetComponent<CameraComponent>().orbitV = go.GetComponent<CameraComponent>().transform.eulerAngles.x;
+        go.GetComponent<CameraComponent>().init_X = go.transform.position.x;
+        go.GetComponent<CameraComponent>().init_Y = go.transform.position.y;
+        go.GetComponent<CameraComponent>().init_Z = go.transform.position.z;
+        //go.GetComponent<CameraComponent>().init_X_angle = go.transform.rotation.x;
+        //go.GetComponent<CameraComponent>().init_Y_angle = go.transform.rotation.y;
+        go.GetComponent<CameraComponent>().initRotation = go.transform.localRotation.eulerAngles;
         //go.GetComponent<CameraComponent>().movementRotation = new Vector3 (Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
 
 	}
@@ -41,37 +47,55 @@ public class CameraSystem : FSystem {
 		foreach( GameObject go in controllableGO ){
 			
 			/*
-			Debug.Log(Input.GetAxis("Horizontal"));
-			if (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0)
+			// Pour placer la caméra derrière un agent
+			//if (Input.GetMouseButtonDown(0))
+			if (Input.GetKeyDown(KeyCode.Space))
 			{
-				if (go.GetComponent<CameraComponent>().limiteCount >= go.GetComponent<CameraComponent>().limiteMin && go.GetComponent<CameraComponent>().limiteCount < go.GetComponent<CameraComponent>().limiteMax)
-				{
-					go.transform.position += new Vector3(go.transform.forward.x + go.transform.up.x, 0, go.transform.forward.z + go.transform.up.z ) * Input.GetAxis("Vertical") * go.GetComponent<CameraComponent>().cameraSpeed * Time.deltaTime;
-					go.transform.position += new Vector3(go.transform.right.x, 0, go.transform.right.z ) * Input.GetAxis("Horizontal") * go.GetComponent<CameraComponent>().cameraSpeed * Time.deltaTime;
-		        	go.GetComponent<CameraComponent>().limiteCount++;
+				var ray = Camera.main.ScreenPointToRay(Input.mousePosition); 
+        		RaycastHit hit = new RaycastHit();
+		        if (Physics.Raycast (ray, out hit))
+		        {
+		            Debug.Log(hit.collider.gameObject.name);
+		            Debug.Log(hit.collider.transform.position);
+		            go.transform.position = new Vector3(hit.collider.transform.position.x, go.transform.position.y, hit.collider.transform.position.z);
+		            //go.transform.Translate(-Input.GetAxisRaw("Mouse X") * Time.deltaTime * go.GetComponent<CameraComponent>().dragSpeed, -Input.GetAxisRaw("Mouse Y") * Time.deltaTime * go.GetComponent<CameraComponent>().dragSpeed, 0);
 		        }
-		    }
-		    */
+			}
+			*/
 
 		    // gauche droite
 		    go.transform.position += new Vector3(go.transform.forward.x + go.transform.up.x, 0, go.transform.forward.z + go.transform.up.z ) * Input.GetAxis("Vertical") * go.GetComponent<CameraComponent>().cameraSpeed * Time.deltaTime;
 			// avance recule
 			go.transform.position += new Vector3(go.transform.right.x, 0, go.transform.right.z ) * Input.GetAxis("Horizontal") * go.GetComponent<CameraComponent>().cameraSpeed * Time.deltaTime;
-			Debug.Log("x = " + go.transform.position.x);
-			Debug.Log("y = " + go.transform.position.y);
-			Debug.Log("z = " + go.transform.position.z);
 			
+			//Debug.Log("position x = " + go.transform.position.x);
+			//Debug.Log("position y = " + go.transform.position.y);
+			//Debug.Log("position z = " + go.transform.position.z);
+			
+			/*
+			Debug.Log("rotation x = " + go.transform.rotation.x);
+			Debug.Log("rotation y = " + go.transform.rotation.y);
+			Debug.Log("rotation z = " + go.transform.rotation.z);
+			*/
+			
+			// Limites de la caméra
+			/*
 			go.transform.position = new Vector3(
 			   Mathf.Clamp(Camera.main.transform.position.x, go.GetComponent<CameraComponent>().MIN_X, go.GetComponent<CameraComponent>().MAX_X),
 			   Mathf.Clamp(Camera.main.transform.position.y, go.GetComponent<CameraComponent>().MIN_Y, go.GetComponent<CameraComponent>().MAX_Y),
 			   Mathf.Clamp(Camera.main.transform.position.z, go.GetComponent<CameraComponent>().MIN_Z, go.GetComponent<CameraComponent>().MAX_Z));
-			
-			/*   
-			go.transform.position = new Vector3(
-			   Mathf.Clamp(go.transform.position.x, go.GetComponent<CameraComponent>().farLeft.position.x, go.GetComponent<CameraComponent>().farRight.position.x),
-			   Mathf.Clamp(go.transform.position.y, go.GetComponent<CameraComponent>().farLeft.position.y, go.GetComponent<CameraComponent>().farRight.position.y),
-			   Mathf.Clamp(go.transform.position.z, go.GetComponent<CameraComponent>().farLeft.position.z, go.GetComponent<CameraComponent>().farRight.position.z));
 			*/
+			
+			float min_x = go.GetComponent<CameraComponent>().init_X+go.GetComponent<CameraComponent>().MIN_X;
+			float max_x = go.GetComponent<CameraComponent>().init_X+go.GetComponent<CameraComponent>().MAX_X;
+			float min_y = go.GetComponent<CameraComponent>().init_Y+go.GetComponent<CameraComponent>().MIN_Y;
+			float max_y = go.GetComponent<CameraComponent>().init_Y+go.GetComponent<CameraComponent>().MAX_Y;
+			float min_z = go.GetComponent<CameraComponent>().init_Z+go.GetComponent<CameraComponent>().MIN_Z;
+			float max_z = go.GetComponent<CameraComponent>().init_Z+go.GetComponent<CameraComponent>().MAX_Z;
+			go.transform.position = new Vector3(
+			   Mathf.Clamp(Camera.main.transform.position.x, min_x, max_x),
+			   Mathf.Clamp(Camera.main.transform.position.y, min_y, max_y),
+			   Mathf.Clamp(Camera.main.transform.position.z, min_z, max_z));
 			//------------------------------------------------------------------------------------
 
 			// Déplacement avec la molette comme dans l'éditeur
@@ -80,6 +104,10 @@ public class CameraSystem : FSystem {
             	Cursor.lockState = CursorLockMode.Locked;
 	        	Cursor.visible = false;
                 go.transform.Translate(-Input.GetAxisRaw("Mouse X") * Time.deltaTime * go.GetComponent<CameraComponent>().dragSpeed, -Input.GetAxisRaw("Mouse Y") * Time.deltaTime * go.GetComponent<CameraComponent>().dragSpeed, 0);
+            	go.transform.position = new Vector3(
+			   Mathf.Clamp(Camera.main.transform.position.x, min_x, max_x),
+			   Mathf.Clamp(Camera.main.transform.position.y, min_y, max_y),
+			   Mathf.Clamp(Camera.main.transform.position.z, min_z, max_z));
             }
 
 			// Zoom avec la molette
@@ -106,7 +134,19 @@ public class CameraSystem : FSystem {
             	go.GetComponent<CameraComponent>().orbitH += go.GetComponent<CameraComponent>().lookSpeedH * Input.GetAxis("Mouse X");
                 go.GetComponent<CameraComponent>().orbitV -= go.GetComponent<CameraComponent>().lookSpeedV * Input.GetAxis("Mouse Y");
                 go.GetComponent<CameraComponent>().transform.eulerAngles = new Vector3(go.GetComponent<CameraComponent>().orbitV, go.GetComponent<CameraComponent>().orbitH, 0f);
-            	//go.transform.position = go.transform.position + new Vector3(0,go.GetComponent<CameraComponent>().orbitH,0);
+
+                /*
+            	Vector3 currentRotation = go.transform.localRotation.eulerAngles; // ne pas modifier
+            	float minRotation_x = go.GetComponent<CameraComponent>().initRotation.x + go.GetComponent<CameraComponent>().MIN_X_angle;
+				float maxRotation_x = go.GetComponent<CameraComponent>().initRotation.x + go.GetComponent<CameraComponent>().MAX_X_angle;
+            	float minRotation_y = go.GetComponent<CameraComponent>().initRotation.y + go.GetComponent<CameraComponent>().MIN_Y_angle;
+				float maxRotation_y = go.GetComponent<CameraComponent>().initRotation.y + go.GetComponent<CameraComponent>().MAX_Y_angle;
+				//currentRotation.x = Mathf.Clamp(currentRotation.x, minRotation_x, maxRotation_x);
+				//currentRotation.y = Mathf.Clamp(currentRotation.y, minRotation_y, maxRotation_y);
+				currentRotation.x = Mathf.Clamp(go.GetComponent<CameraComponent>().orbitV, minRotation_x, maxRotation_x);
+				currentRotation.y = Mathf.Clamp(go.GetComponent<CameraComponent>().orbitH, minRotation_y, maxRotation_y);
+				go.transform.localRotation = Quaternion.Euler (currentRotation); // ne pas modifier
+				*/
             }
 	        else
 	        {
