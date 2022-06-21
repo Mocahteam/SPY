a0441c75a041ad2fe01a1c949da6fa69beb48150
Vaerysse <seb.maillos@gmail.com>
2022-06-21 18:43:51 +0200

    Debut creation panel selection competence

---
+++ Assets/Systems/ParamCompetenceSystem.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using FYFY;
+
+public class ParamCompetenceSystem : FSystem {
+
+	public static ParamCompetenceSystem instance;
+
+	public ParamCompetenceSystem()
+	{
+		instance = this;
+	}
+	
+}
