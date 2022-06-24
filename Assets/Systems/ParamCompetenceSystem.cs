c1ff8915c7f0b2e3723c47fb9da00e9f0221b304
Vaerysse <seb.maillos@gmail.com>
2022-06-24 15:34:01 +0200

    parametrage des competences via csv

--- Assets/Systems/ParamCompetenceSystem.cs
+++ Assets/Systems/ParamCompetenceSystem.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using FYFY;
 using TMPro;
+using System.IO;
 
 public class ParamCompetenceSystem : FSystem
 {
@@ -21,6 +22,7 @@ public class ParamCompetenceSystem : FSystem
 
 	protected override void onStart()
 	{
+		// On désactive les compétences pas encore implémenté
 		foreach(GameObject comp in competence_f)
         {
             if (!comp.GetComponent<Competence>().active)
@@ -28,6 +30,40 @@ public class ParamCompetenceSystem : FSystem
 				comp.GetComponent<Toggle>().interactable = false;
 			}
         }
+		// On charge les données pour chaque compétence
+		try
+        {
+			StreamReader reader = new StreamReader(Application.dataPath + "/StreamingAssets/ParamPrefab/ParamCompetence.csv");
+
+			bool endOfFile = false;
+            while (!endOfFile)
+            {
+				string data_string = reader.ReadLine();
+				if (data_string == null)
+				{
+					endOfFile = true;
+					break;
+				}
+				var data = data_string.Split(';');
+
+				// On parcourt les élément est on charge les parametre de la compétence sur la bonne compétence
+				// Nom de la competence, titre, info
+				foreach (GameObject comp in competence_f)
+				{
+					if (comp.name == data[0])
+					{
+						comp.transform.Find("Label").GetComponent<Text>().text = data[1];
+						comp.GetComponent<Competence>().info = data[2];
+					}
+				}
+			}
+		}
+        catch
+        {
+			Debug.Log("Erreur chargement fichier de parametrage des compétences");
+        }
+
+
 	}
 
 	public void startLevel()
