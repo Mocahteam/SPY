55e0f9d19b9c832e89b9528759e8a4670bf23369
Muratet <mathieu.muratet@gmail.com>
2022-09-29 17:37:49 +0200

    Add doorPath prefab Add levelModel XML file

---
+++ Assets/Components/DoorPath.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+
+public class DoorPath : MonoBehaviour {
+	// Advice: FYFY component aims to contain only public members (according to Entity-Component-System paradigm).
+	public int slotId;
+}
